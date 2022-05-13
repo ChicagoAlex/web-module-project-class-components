c8bfe885bc3bcda8bdc075c88e268f5dddf483e6
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-05-13 13:45:42 -0500

    adding in DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <ul>
+          <h1>My Todo App</h1>
+          <li>Go for walk</li>
+          <li>Laundry</li>
+          <li>Wash dishes</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Add todo</button>
+        </form>
+
+        <button>Clear completed</button>
       </div>
     )
   }
