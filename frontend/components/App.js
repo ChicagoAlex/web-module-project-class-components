575fc50cc605d1292e6b9deb485c8e5023632233
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-05-13 14:23:00 -0500

    build out state, added mock data

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,37 @@
+/* eslint-disable react/jsx-key */
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    const { todos } = this.state;
     return (
       <div>
+        <h1>My todo list</h1>
+
         <ul>
-          <h1>My Todo App</h1>
-          <li>Go for walk</li>
-          <li>Laundry</li>
-          <li>Wash dishes</li>
+          {
+            todos.map(todo=> {
+              return (<li>{todo.name} { todo.completed?<span>- completed</span> : <span></span> }</li>)
+            })
+          }       
         </ul>
 
         <form>
